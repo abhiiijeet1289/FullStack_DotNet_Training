aa7a101f180f00193a07ff710e28a20aaa26f3b0
Abhijeet Suryawanshi <132201474+abhiiijeet1289@users.noreply.github.com>
2025-08-23 00:08:41 +0530

    Delete 06_Angular directory

--- 06_Angular/03_Building_Blocks/angular-building-blocks/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-
-export const routes: Routes = [
-  { path: '', component: WelcomeComponent }
-];
--- 06_Angular/README.md
+++
@@ -1,2 +0,0 @@
-# FullStack_DotNet_Training
-Code and projects from Full Stack .NET Training (C#, .NET Core, SQL, Angular, Web Dev, etc.)
