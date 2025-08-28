7d723b7acd15cd864b7aea8dbf2087ea2b733822
Abhijeet Suryawanshi <abhijeet.suryawanshi742@gmail.com>
2025-08-28 21:52:45 +0530

    Added 02_Testing Frameworks with NUnit, MSTest, xUnit examples

---
+++ 08_UnitTesting_TDD/02_TestingFrameworks/CalculatorLib.MSTests/CalculatorMSTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLib;
+
+
+namespace CalculatorLib.MSTests
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        private Calculator _calc;
+
+        [TestInitialize]
+        public void Init() => _calc = new Calculator();
+
+        [TestMethod]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            Assert.AreEqual(9, _calc.Add(5, 4));
+        }
+    }
+}
---
+++ 08_UnitTesting_TDD/02_TestingFrameworks/CalculatorLib.XUnitTests/CalculatorXUnitTests.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorLib.XUnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
