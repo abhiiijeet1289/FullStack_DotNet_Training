58e2803c4f8f2452a3390f38cbe5a6750300a3b9
Abhijeet Suryawanshi <abhijeet.suryawanshi742@gmail.com>
2025-08-28 21:35:31 +0530

    Added 01_Introduction to Unit Testing with NUnit example

---
+++ 08_UnitTesting_TDD/01_Introduction_UnitTesting/CalculatorLib.Tests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using CalculatorLib;
+using Assert = NUnit.Framework.Assert;
+
+namespace CalculatorLib.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calc = new Calculator();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnsSum()
+        {
+            var result = _calc.Add(2, 3);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Subtract_WhenCalled_ReturnsDifference()
+        {
+            var result = _calc.Subtract(5, 3);
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
